9976a5d3cc5b8f84e11deaa3d6e4942936cb5e9b
SDhananjaya0159 <k.k.s.dhananjaya@gmail.com>
2026-04-05 13:17:01 +0530

    New Features

---
+++ frontend/src/data/modules.js
@@ -0,0 +1,12 @@
+export const ALL_MODULES = [
+  { id: 'm1', code: 'IT1010', name: 'Fundamentals of Programming', year: 1, semester: 1 },
+  { id: 'm2', code: 'IT1020', name: 'Computer Systems', year: 1, semester: 1 },
+  { id: 'm3', code: 'IT1030', name: 'Mathematics for Computing', year: 1, semester: 1 },
+  { id: 'm4', code: 'IT1040', name: 'Software Engineering', year: 1, semester: 2 },
+  { id: 'm5', code: 'IT1050', name: 'Database Management Systems', year: 1, semester: 2 },
+  { id: 'm6', code: 'IT2010', name: 'Object Oriented Programming', year: 2, semester: 1 },
+  { id: 'm7', code: 'IT2020', name: 'Data Structures and Algorithms', year: 2, semester: 1 },
+  { id: 'm8', code: 'IT2030', name: 'Web Technologies', year: 2, semester: 2 },
+  { id: 'm9', code: 'IT3010', name: 'Framework Based Web Dev', year: 3, semester: 1 },
+  { id: 'm10', code: 'IT3020', name: 'Mobile App Development', year: 3, semester: 2 },
+];
